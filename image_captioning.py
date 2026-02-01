9ebbbd080d7983e28a6ce01b64d702066d5747a5
Chinmai13 <23211a1247@bvrit.ac.in>
2026-02-01 22:19:48 +0530

    Image captioning functionality
    Implement image captioning using VisionEncoderDecoderModel.

---
+++ image_captioning.py
@@ -0,0 +1,14 @@
+from transformers import VisionEncoderDecoderModel, ViTImageProcessor, AutoTokenizer
+from PIL import Image
+import torch
+
+model = VisionEncoderDecoderModel.from_pretrained("nlpconnect/vit-gpt2-image-captioning")
+processor = ViTImageProcessor.from_pretrained("nlpconnect/vit-gpt2-image-captioning")
+tokenizer = AutoTokenizer.from_pretrained("nlpconnect/vit-gpt2-image-captioning")
+
+image = Image.open("image.jpg").convert("RGB")
+pixel_values = processor(images=image, return_tensors="pt").pixel_values
+
+output_ids = model.generate(pixel_values, max_length=16)
+caption = tokenizer.decode(output_ids[0], skip_special_tokens=True)
+print(caption)
